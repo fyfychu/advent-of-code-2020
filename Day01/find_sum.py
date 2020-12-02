5c9cf6774b633e2eb208b266198bef78877685d1
Fang-Ying Chu <fang.ying.chu@gmail.com>
2020-12-01 23:50:26 -0800

    Modified Day01 to return None instead of dummy values when sum not found and try/except printing results + modified naming of index to reflect order in array

--- Day01/find_sum.py
+++ Day01/find_sum.py
@@ -13,7 +13,7 @@ numbers.sort()
 def find_pair_for_target_sum(target_sum, numbers):
     for index, number in enumerate(numbers):
         if number > target_sum:
-            return -1, number
+            return
         else:
             second_index = len(numbers) - index - 1
             while second_index > index:
@@ -25,32 +25,36 @@ def find_pair_for_target_sum(target_sum, numbers):
                     break
                 else:
                     second_index -= 1
-    return -1, 1
 
 
 def find_triple_for_target_sum(target_sum, numbers):
     for index, number in enumerate(numbers):
         if number > target_sum:
-            return -1, number
+            return
         else:
-            second_index = len(numbers) - index - 1
-            while second_index > index:
-                second_number = numbers[second_index]
-                third_index = second_index - 1
-                while third_index > index:
-                    third_number = numbers[third_index]
+            third_index = len(numbers) - index - 1
+            while third_index > index:
+                third_number = numbers[third_index]
+                second_index = third_index - 1
+                while second_index > index:
+                    second_number = numbers[second_index]
                     this_sum = number + second_number + third_number
                     if this_sum == target_sum:
                         return number, second_number, third_number
                     if this_sum < target_sum:
                         break
                     else:
-                        third_index -= 1
-                second_index -= 1
-    return -1, 1
+                        second_index -= 1
+                third_index -= 1
 
-number, second_number = find_pair_for_target_sum(int(target_sum), numbers)
-print(f'{number}*{second_number} = {number*second_number}')
+try:
+    number, second_number = find_pair_for_target_sum(int(target_sum), numbers)
+    print(f'{number}*{second_number} = {number*second_number}')
+except:
+    print(f'Cannot sum to {target_sum} with tuple')
 
-number, second_number, third_number = find_triple_for_target_sum(int(target_sum), numbers)
-print(f'{number}*{second_number}*{third_number} = {number*second_number*third_number}')
+try:
+    number, second_number, third_number = find_triple_for_target_sum(int(target_sum), numbers)
+    print(f'{number}*{second_number}*{third_number} = {number*second_number*third_number}')
+except:
+    print(f'Cannot sum to {target_sum} with triple')
