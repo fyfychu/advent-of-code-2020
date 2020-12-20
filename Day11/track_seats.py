d934adb5c3bd69d20b0d9eb15094a9e6b40e3453
Fang-Ying Chu <fang.ying.chu@gmail.com>
2020-12-20 14:32:11 -0800

    Day11 rough solution

--- Day11/track_seats.py
+++ Day11/track_seats.py
@@ -12,6 +12,96 @@ with open(file_name, 'r') as file:
 
 print(area)
 
+print_x = 2
+print_y = 0
+def modify_seat_directional(x, y, area):
+    seat = area[x][y]
+    num_occupied_seats = 0
+    for i in reversed(range(0, x)):
+        if i >= 0 and area[i][y] == '#':
+            num_occupied_seats += 1
+            break
+        elif i >= 0 and area[i][y] == 'L':
+            break
+
+    for i in range(x+1, len(area)):
+        if i < len(area) and area[i][y] == '#':
+            num_occupied_seats += 1
+            break
+        elif i < len(area) and area[i][y] == 'L':
+            break
+
+    for j in reversed(range(0, y)):
+        if j >= 0 and area[x][j] == '#':
+            num_occupied_seats += 1
+            break
+        elif j >= 0 and area[x][j] == 'L':
+            break
+
+    for j in range(y+1, len(area[0])):
+        if j < len(area[0]) and area[x][j] == '#':
+            num_occupied_seats += 1
+            break
+        elif j < len(area[0]) and area[x][j] == 'L':
+            break
+
+    try:
+        j = y
+        for i in reversed(range(0, x)):
+            j -= 1
+            if i >= 0 and j >= 0 and area[i][j] == '#':
+                num_occupied_seats += 1
+                raise Exception
+            elif i >= 0 and j >= 0 and area[i][j] == 'L':
+                raise Exception
+    except:
+        pass
+
+    try:
+        j = y
+        for i in reversed(range(0, x)):
+            j += 1
+            if i >= 0 and j < len(area[0]) and area[i][j] == '#':
+                num_occupied_seats += 1
+                raise Exception
+            elif i >= 0 and j < len(area[0]) and area[i][j] == 'L':
+                raise Exception
+    except:
+        pass
+
+    try:
+        j = y
+        for i in range(x+1, len(area)):
+            j += 1
+            if i < len(area) and j < len(area[0]) and area[i][j] == '#':
+                num_occupied_seats += 1
+                raise Exception
+            elif i < len(area) and j < len(area[0]) and area[i][j] == 'L':
+                raise Exception
+    except:
+        pass
+
+    try:
+        j = y
+        for i in range(x + 1, len(area)):
+            j -= 1
+            if i < len(area) and j >= 0 and area[i][j] == '#':
+                num_occupied_seats += 1
+                raise Exception
+            elif i < len(area) and j >= 0 and area[i][j] == 'L':
+                raise Exception
+    except:
+        pass
+
+    if x == print_x and y == print_y:
+        print(f'{x} {y} has {num_occupied_seats} SEATS')
+
+    if num_occupied_seats >= 5 and seat == '#':
+        return 'L'
+    elif num_occupied_seats == 0 and seat == 'L':
+        return '#'
+    else:
+        return seat
 
 # seat empty + no occupied seats -> sit
 # seat occupied with 4+ seats around -> empty
@@ -62,7 +152,8 @@ def solve():
         prev_area_iter = deepcopy(next_area_iter)
         for x in range(0, len(area)):
             for y in range(0, len(area[0])):
-                new_position = modify_seat(x, y, prev_area_iter)
+                # new_position = modify_seat(x, y, prev_area_iter)
+                new_position = modify_seat_directional(x, y, prev_area_iter)
                 next_area_iter[x][y] = new_position
         iter_count += 1
         print(f'ITER COUNT {iter_count}')
